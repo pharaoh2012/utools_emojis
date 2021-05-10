b34a980790974d2c9514d74ad38e084a95cd48b3
rm <rm@cqkcy.com>
2021-05-10 16:52:22 +0800

    增加点击次数排序，点击次数越多，排序越靠前。

--- preload.js
+++ preload.js
@@ -13,18 +13,17 @@ changeStyle = () => {
         font-size: 1rem;
         padding-left: 10px;
     `);
-    document.head.appendChild(style); 
+    document.head.appendChild(style);
 }
 
-// searchEmoji = (emoji, search) => {
-//     let kw = search.toLowerCase();
-//     for (var keyword of emoji.keywords) {
-//         if (keyword.toLowerCase().includes(kw)) {
-//             return true
-//         }
-//     }
-//     return false
-// }
+let emojis_code, emojis;
+let emojesClickCount;
+
+utools.onPluginReady(() => {
+    changeStyle();
+})
+
+
 
 // 列表模式
 window.exports = {
@@ -32,8 +31,23 @@ window.exports = {
         mode: "list",
         args: {
             enter: async (action, callbackSetList) => {
-                changeStyle();
-                emojis = require('./emojis.json')
+                //changeStyle();
+                if (!emojis) {
+                    console.log('init emojis')
+                    emojis = require('./emojis.json')
+                    emojesClickCount = utools.db.get("emojesClickCount");
+                    if(!emojesClickCount) emojesClickCount = {
+                        _id:"emojesClickCount",
+                        c:{}
+                    }
+                    emojis.forEach(e => {
+                        e.title = e._id;
+                        e.keyword += e._id;
+                        e.c = emojesClickCount.c[e._id]??0;
+                    });
+                    emojis.sort((a,b)=>b.c-a.c);
+                }
+
                 callbackSetList(emojis)
             },
             search: (action, searchWord, callbackSetList) => {
@@ -41,25 +55,36 @@ window.exports = {
                 if (!searchWord) return callbackSetList(emojis);
                 let kw = searchWord.toLowerCase().split(' ');
                 let sRet = emojis.filter(x => {
-                    for(let k of kw) {
-                        if(k) {
-                            if(!x.keyword.includes(k)) return false;
+                    for (let k of kw) {
+                        if (k) {
+                            if (!x.keyword.includes(k)) return false;
                         }
                     }
                     return true;
                 });
-                if(/^[a-zA-Z]+$/.test(searchWord)) {
+                if (/^[a-zA-Z]+$/.test(searchWord)) {
                     //纯粹英文进行排序
-                    let reg = new RegExp("\\b"+searchWord+"\\b","i");
-                    sRet.sort((a,b)=>reg.test(a.keyword)?-1:1);
+                    let reg = new RegExp("\\b" + searchWord + "\\b", "i");
+                    sRet.sort((a, b) => {
+                        if(a.c == b.c)   return reg.test(a.keyword) ? -1 : 1;
+                        return b.c-a.c;
+                    });
                 }
                 callbackSetList(sRet);
             },
             select: async (action, itemData, callbackSetList) => {
-                utools.copyText(itemData.title);
-                //utools.copyText(JSON.stringify(action));
+                utools.copyText(itemData._id);
+                itemData.c++;
+
                 utools.hideMainWindow();
                 utools.simulateKeyboardTap('v', utools.isWindows() ? 'ctrl' : 'command')
+                emojis.sort((a,b)=>b.c-a.c);
+                //将点击次数保存到数据库。
+                if(itemData.c<50000) {
+                    emojesClickCount.c[itemData._id]=itemData.c;
+                    utools.db.put(emojesClickCount);
+                }
+
             },
             placeholder: "搜索，回车发送到活动窗口"
         }
@@ -68,18 +93,21 @@ window.exports = {
         mode: "list",
         args: {
             enter: async (action, callbackSetList) => {
-                changeStyle();
-                emojis = require('./emojiscode.json')
-                callbackSetList(emojis)
+                //changeStyle();
+                if (!emojis_code) {
+                    console.log('init emojis_code')
+                    emojis_code = require('./emojiscode.json')
+                }
+                callbackSetList(emojis_code)
             },
             search: (action, searchWord, callbackSetList) => {
                 searchWord = searchWord.trim();
-                if (!searchWord) return callbackSetList(emojis);
+                if (!searchWord) return callbackSetList(emojis_code);
                 let kw = searchWord.toLowerCase().split(' ');
-                callbackSetList(emojis.filter(x => {
-                    for(let k of kw) {
-                        if(k) {
-                            if(!x.keyword.includes(k)) return false;
+                callbackSetList(emojis_code.filter(x => {
+                    for (let k of kw) {
+                        if (k) {
+                            if (!x.keyword.includes(k)) return false;
                         }
                     }
                     return true;
